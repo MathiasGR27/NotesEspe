e1151bb44f0c524cb3c63aeac8a63c4e43768718
megualpa <megualpa@espe.edu.ec>
2025-08-05 19:48:53 -0500

    feat:Actualizacion del archivo sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,26 @@
 self.addEventListener('install', (event) => {
-    console.info("Service Worker installed");
+    console.log("Almacenando archivos en cachee....ESPE");
+    const wu = new Promise((resolve, reject) => {
+        try {
+            setTimeout(() => {
+                const addFiles = ""; // Aquí puedes agregar los archivos que deseas almacenar en caché
+                console.log("Service Worker installed Espe");
+                resolve();
+            }, 1000);
+            self.skipWaiting(); // Forzar la activación inmediata del Service Worker
+        } catch (error) {
+            reject(error);
+        }
+        event.waitUntil(wu); // Asegura que la instalación se complete antes de activar el Service Worker
+    });
 });
 
 self.addEventListener('activate', (event) => {
-    console.info("Service Worker activated");
+    console.log("Service Worker activated");
+    event.waitUntil(clients.claim()); // Asegura que el Service Worker tome el control de las páginas abiertas
 });
 
 self.addEventListener('fetch', (event) => {
-    console.info("Service Worker fetching:");
+    console.log("Cacheando claims");
+    console.log(event.request.url);
 });
-
-
